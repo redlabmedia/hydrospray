23721245c695b5ad7a942bd21636101ed5e7b611
sugapablo <russ@sugapablo.com>
2018-04-27 15:25:28 -0400

    js 8

--- rlm_x1/static/src/js/custom1.js
+++ rlm_x1/static/src/js/custom1.js
@@ -1,22 +1,27 @@
 /* insert custom javascript here */
-odoo.rlm_x1 = function(instance){
-    instance.website.contentMenu.EditMenuDialog.include({
-        start: function () {
-            var r = this._super.apply(this, arguments);
-            this.$('.oe_menu_editor').nestedSortable({
-                listType: 'ul',
-                handle: 'div',
-                items: 'li',
-                maxLevels: 5,
-                toleranceElement: '> div',
-                forcePlaceholderSize: true,
-                opacity: 0.6,
-                placeholder: 'oe_menu_placeholder',
-                tolerance: 'pointer',
-                attribute: 'data-menu-id',
-                expression: '()(.+)', // nestedSortable takes the second match of an expression (*sigh*)
-            });
-            return r;
-        },
-    });
-};
+odoo.define('rlm_x1.js_name', function (require) {
+	'use strict';
+
+	// assign the variable EditMenuDialog of website module's contentModule
+	// js in a variable
+	var EditMenuDialog = require('website.contentMenu').EditMenuDialog;
+
+	// for modifying use the .include function
+	EditMenuDialog.include({
+		start: function () {
+			this.$('.oe_menu_editor').nestedSortable({
+				listType: 'ul',
+				handle: 'div',
+				items: 'li',
+				maxLevels: 5, // changed maxLevels from 2 to 3
+				toleranceElement: '> div',
+				forcePlaceholderSize: true,
+				opacity: 0.6,
+				placeholder: 'oe_menu_placeholder',
+				tolerance: 'pointer',
+				attribute: 'data-menu-id',
+				expression: '()(.+)'
+			});
+		}
+	});
+});
