1a188f1d4337b14ad2da28b9b39ae271672a019f
sugapablo <russ@sugapablo.com>
2018-04-27 08:47:07 -0400

    js 1

--- rlm_x1/static/src/js/custom1.js
+++ rlm_x1/static/src/js/custom1.js
@@ -1,1 +1,24 @@
 /* insert custom javascript here */
+openerp.new_module = function(instance){
+    var module = instance.website // loading the namespace of the 'sample' module
+
+    module.website.contentMenu.EditMenuDialog.include({
+        start: function () {
+            var r = this._super.apply(this, arguments);
+            this.$('.oe_menu_editor').nestedSortable({
+                listType: 'ul',
+                handle: 'div',
+                items: 'li',
+                maxLevels: 5,
+                toleranceElement: '> div',
+                forcePlaceholderSize: true,
+                opacity: 0.6,
+                placeholder: 'oe_menu_placeholder',
+                tolerance: 'pointer',
+                attribute: 'data-menu-id',
+                expression: '()(.+)', // nestedSortable takes the second match of an expression (*sigh*)
+            });
+            return r;
+        },
+    });
+};
